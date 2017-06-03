7076362d3d3b345052d5ac5ec234dc1063b99cf1
oscar <ramajooscar@gmail.com>
2017-06-03 19:18:56 +0200

    splash

---
+++ app/src/main/java/com/example/oscar/teammanager/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.oscar.teammanager;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends Activity {
+
+    // Set the duration of the splash screen
+    private static final long SPLASH_SCREEN_DELAY = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                // Start the next activity
+                Intent mainIntent = new Intent().setClass(SplashActivity.this, LoginActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        };
+
+        // Simulate a long loading process on application startup.
+        Timer timer = new Timer();
+        timer.schedule(task, SPLASH_SCREEN_DELAY);
+    }
+
+}
